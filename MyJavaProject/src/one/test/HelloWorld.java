aea58c9e2e5eb6fbf5be438467a6135601910856
anjan <anjan@LAPTOP-2S8BC3KU>
2023-02-03 21:36:52 +0530

    first upload

---
+++ MyJavaProject/src/one/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package one.test;
+
+//import java.util.Scanner;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+    //System.out.println("Hello World");
+    HelloWorld.display();
+    
+	}
+public static void display() {
+	 System.out.println("Hello World");
+}
+}
